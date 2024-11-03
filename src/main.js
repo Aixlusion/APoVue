51c9642ac24550be41f7d63794fe7ea6681a468a
Aixlusion <hungtuanvu71246@gmail.com>
2024-11-04 00:26:52 +0700

    Fixes #5 (Final)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 import './pfp.jpg'
 import './gif1.gif'
 import { createApp } from 'vue';
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css';
 
