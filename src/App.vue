32bc22a74bb872ad98961da60d95132bb3bc5252
Aixlusion <hungtuanvu71246@gmail.com>
2024-11-03 22:27:20 +0700

    Initialize APW Repos

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Profile />
+  </div>
+</template>
+
+<script>
+import Profile from "./components/Profile.vue";
+export default {
+  components: {
+    Profile,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css';
+
+createApp(App).mount('#app')
